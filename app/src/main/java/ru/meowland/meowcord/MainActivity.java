734ac294a2278f9ce028a81d8c1542c8ef5539e4
KotMilkMeoWtwo <forgithubkotmilkmeow@github.com>
2022-03-18 20:28:33 +0300

    ааааааааааааааа

--- app/src/main/java/ru/meowland/meowcord/MainActivity.java
+++ app/src/main/java/ru/meowland/meowcord/MainActivity.java
@@ -1,33 +1,99 @@
 package ru.meowland.meowcord;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SING_IN_CODE = 1;
+    private RelativeLayout activity_main;
+    private FloatingActionButton sendButton;
+    private FirebaseListAdapter<Message> adapter;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        sendButton = findViewById(R.id.buttonSend);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText textField = findViewById(R.id.messageField);
+                if(textField.getText().toString().equals("")){
+                    return;
+                }
+                FirebaseDatabase.getInstance().getReference().push().setValue(
+                        new Message(
+                        FirebaseAuth.getInstance().getCurrentUser().getDisplayName(),
+                        textField.getText().toString()
+                    )
+                );
+                textField.setText("");
+            }
+
+        });
+
+        if(requestCode == SING_IN_CODE){
+            if(requestCode == RESULT_OK){
+                Snackbar.make(activity_main, "You sing in now", Snackbar.LENGTH_SHORT).show();
+                displayALLMessages();
+            }else {
+                Snackbar.make(activity_main, "You not sing in now", Snackbar.LENGTH_SHORT).show();
+                finish();
+            }
+
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activity_main = findViewById(R.id.main_activity);
+        if(FirebaseAuth.getInstance().getCurrentUser()==null){
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SING_IN_CODE);
+        }
+        else{
+            Snackbar.make(activity_main, "You sing in now", Snackbar.LENGTH_SHORT).show();
+
+            displayALLMessages();
+        }
+    }
+
+    private void displayALLMessages() {
+        ListView listMessages = findViewById(R.id.messages_list);
+        adapter = new FirebaseListAdapter<Message>(this, Message.class, R.layout.list_item, FirebaseDatabase.getInstance().getReference()){
+            @Override
+            protected void populateView(@NonNull View v, @NonNull Message model, int position) {
+                TextView mess_user, mess_time, mess_text;
+                mess_user = v.findViewById(R.id.message_user);
+                mess_time = v.findViewById(R.id.message_time);
+                mess_text = v.findViewById(R.id.message_text);
+
+                mess_user.setText(model.getNickName());
+                mess_text.setText(model.getTextMessage());
+                mess_time.setText(DateFormat.format("dd-mm-yyyy HH:mm:ss", model.getMessageTime()));
+            }
+        };
+
+        listMessages.setAdapter(adapter);
     }
     /*
     Button button;
